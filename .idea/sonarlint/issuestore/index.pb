630c8d58d6a5d6ce8c88d9e312614fd260b4755e
Rafal Jaszczynski <rafal_jaszczynski@epam.com>
2019-01-09 18:56:07 +0100

    It fu!@ing works !!!!!!!!!!!!!!!!!

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,11 @@
 
 P
  src/ResizeProjectViewAction.java,5/5/55f3b76abf33077443b61dcfb9f9626741b5a211
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+K
+tool-window-auto-resize.iml,b/c/bcd54df140bfbb4d1891c9bb1b30f2a1b36770a8
+M
+resources/META-INF/plugin.xml,9/1/916c178f3f2e3f9c290ddc27914107cdb9b953e9
+L
+src/ProjectViewGetter.groovy,9/2/92797a2a988aa99870b4a53bda674458e1308504
--- src/ResizeProjectViewAction.java
+++ src/ResizeProjectViewAction.java
@@ -8,8 +8,10 @@ import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.openapi.wm.ex.ToolWindowEx;
 
 import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
-import java.awt.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class ResizeProjectViewAction extends AnAction {
     
@@ -27,30 +29,57 @@ public class ResizeProjectViewAction extends AnAction {
         AbstractProjectViewPane currentProjectViewPane = projectView.getCurrentProjectViewPane();
         
         JTree tree = currentProjectViewPane.getTree();
-        
-        DefaultMutableTreeNode root = (DefaultMutableTreeNode) tree.getModel()
-                                                                   .getRoot();
-        
-        System.out.println("root.getClass() = " + root.getClass());
-        
-        Container parent = currentProjectViewPane.getTree()
-                                                 .getParent();
-        
-        Component component = parent.getComponent(0);
-        
-        //TreeNode child = root.getChild(parent, 1);
-        
-        System.out.println("projectView.getCurrentProjectViewPane()\n                   .getTree()\n                 "
-                               + "  " + ".getAccessibleContext()\n                   .getAccessibleTable() = " + projectView.getCurrentProjectViewPane()
-                                                                                                                                                                                                                    .getTree()
-                                                                                                                                                                                                                    .getAccessibleContext()
-                                                                                                                                                                                                                    .getAccessibleTable());
-        
-        int desiredWidth = 200;
+    
+        System.out.println("tree.getRowCount() = " + tree.getRowCount());
+    
+        System.out.println("-----------------");
+        System.out.println("Paths with for parents");
+        System.out.println("-----------------");
+    
+        IntStream.range(0, tree.getRowCount())
+                 .forEach(value -> System.out.println(value + " name= " + tree.getPathForRow(value)
+                                                                              .getParentPath()
+                                                                              .toString()));
+    
+        System.out.println("-----------------");
+        System.out.println("Path for childs");
+        System.out.println("-----------------");
+    
+        IntStream.range(0, tree.getRowCount())
+                 .forEach(value -> System.out.println(value + " name= " + tree.getPathForRow(value)
+                                                                              .toString()));
+    
+        System.out.println("-----------------");
+    
+        int desiredWidth = tree.getPathBounds(tree.getPathForRow(9)).width + tree.getPathBounds(tree.getPathForRow(9)).x;
+        System.out.println("desiredWidth = " + desiredWidth);
         
         int width = tw.getComponent()
                       .getWidth();
         tw.stretchWidth(desiredWidth - width);
+    
+        longestPathValueOfTree(tree);
+    }
+    
+    int longestPathValueOfTree(JTree tree) {
+        List<Integer> list = IntStream.range(0, tree.getRowCount())
+                                      .map(rowIndex -> tree.getPathForRow(rowIndex)
+                                                           .toString()
+                                                           .length())
+                                      .boxed()
+                                      .collect(toList());
+        
+        System.out.println("List length:");
+        list.forEach(System.out::println);
+        
+        Integer max = list.get(0);
+        for (final Integer integer : list) {
+            if (integer > max) {
+                max = integer;
+            }
+        }
+        
+        return list.indexOf(max);
     }
     
 }
